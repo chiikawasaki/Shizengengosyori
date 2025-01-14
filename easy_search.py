4ada75dbb191e2124bc3a576c0992eb51f9348af
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-01-14 20:17:24 +0900

    add: easy_search.py

---
+++ easy_search.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import glob
+import os
+import sys
+  
+            
+if __name__ == "__main__":
+    args = sys.argv
+    # 検索キーワードを取得
+    keyword = args[1]
+
+    # 転置インデックスを読み込む
+    index_file = open('index.txt','r')
+    index_hash = {}
+    # indecx_hashに格納
+    for line in index_file:
+        # 改行を除去
+        line = line.rstrip()
+        line = line.split(' ')
+        # 名詞をキー、記事名と出現回数を値とする辞書型オブジェクト
+        index_hash.setdefault(line[0],line[1])
+    
+    # 検索キーワードが含まれる記事を検索
+    for key in index_hash:
+        if keyword in key:
+            print(f"{key} {index_hash[key]}")
